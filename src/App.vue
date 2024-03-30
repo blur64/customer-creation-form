5a289dc55bfd83a8e26130e314c6e37455e1c9d2
blur64 <shalomsi922@gmail.com>
2024-03-30 17:21:01 +0400

    add form state, v-model it with html inputs

--- src/App.vue
+++ src/App.vue
@@ -6,85 +6,89 @@
         <legend><h2>Основная информация</h2></legend>
 
         <label for="surname">Фамилия</label>
-        <input id="surname" type="text" />
+        <input v-model="main.surname" id="surname" type="text" />
 
         <label for="name">Имя</label>
-        <input id="name" type="text" />
+        <input v-model="main.name" id="name" type="text" />
 
         <label for="patronymic">Отчество</label>
-        <input id="patronymic" type="text" />
+        <input v-model="main.patronymic" id="patronymic" type="text" />
 
         <label for="birth-date">Дата рождения</label>
-        <input id="birth-date" type="date" />
+        <input v-model="main.birthDate" id="birth-date" type="date" />
 
         <label for="sex">Пол</label>
-        <select id="sex">
+        <select v-model="main.sex" id="sex">
           <option value="0">М</option>
           <option value="1">Ж</option>
         </select>
 
         <label for="phone-number">Номер телефона</label>
-        <input id="phone-number" type="tel" />
+        <input v-model="main.phoneNumber" id="phone-number" type="tel" />
 
         <label for="assigned-doctor">Лечащий врач</label>
-        <select id="assigned-doctor">
+        <select v-model="main.assignedDoctor" id="assigned-doctor">
           <option value="0">Иванов</option>
           <option value="1">Захаров</option>
           <option value="2">Чернышева</option>
         </select>
 
         <label for="clents-group">Группа клиентов</label>
-        <select id="clents-group" multiple>
+        <select v-model="main.clientsGroup" id="clents-group" multiple>
           <option value="0">VIP</option>
           <option value="1">Проблемные</option>
           <option value="2">ОМС</option>
         </select>
 
-        <input id="is-send-texting" type="checkbox" />
-        <label for="is-send-texting">Не отправлять СМС</label>
+        <input
+          v-model="main.isNotSendTexting"
+          id="is-not-send-texting"
+          type="checkbox"
+        />
+        <label for="is-not-send-texting">Не отправлять СМС</label>
       </fieldset>
       <fieldset>
         <legend><h2>Адрес</h2></legend>
 
         <label for="city">Город</label>
-        <input id="city" type="text" />
+        <input v-model="address.city" id="city" type="text" />
 
         <label for="country">Страна</label>
-        <input id="country" type="text" />
+        <input v-model="address.country" id="country" type="text" />
 
         <label for="region">Область</label>
-        <input id="region" type="text" />
+        <input v-model="address.region" id="region" type="text" />
 
         <label for="postcode">Индекс</label>
-        <input id="postcode" type="text" />
+        <input v-model="address.postcode" id="postcode" type="text" />
 
         <label for="house">Дом</label>
-        <input id="house" type="text" />
+        <input v-model="address.house" id="house" type="text" />
 
         <label for="street">Улица</label>
-        <input id="street" type="text" />
+        <input v-model="address.street" id="street" type="text" />
       </fieldset>
       <fieldset>
         <legend><h2>Паспортные данные</h2></legend>
 
         <label for="document-type">Тип документа</label>
-        <select id="document-type">
+        <select v-model="docs.type" id="document-type">
           <option value="0">Паспорт</option>
           <option value="1">Свидетельство о рождении</option>
           <option value="2">Вод. удостоверение</option>
         </select>
 
         <label for="document-series">Серия</label>
-        <input id="document-series" type="number" />
+        <input v-model="docs.series" id="document-series" type="number" />
 
         <label for="document-number">Номер</label>
-        <input id="document-number" type="number" />
+        <input v-model="docs.number" id="document-number" type="number" />
 
         <label for="document-issuer">Кем выдан</label>
-        <input id="document-issuer" type="text" />
+        <input v-model="docs.issuer" id="document-issuer" type="text" />
 
         <label for="document-issue-date">Дата выдачи</label>
-        <input id="document-issue-date" type="date" />
+        <input v-model="docs.issueDate" id="document-issue-date" type="date" />
       </fieldset>
       <button type="submit">Сохранить</button>
     </form>
@@ -100,7 +104,34 @@ export default {
     return { v$: useVuelidate() };
   },
   data() {
-    return {};
+    return {
+      main: {
+        surname: "",
+        name: "",
+        patronymic: "",
+        birthDate: new Date(0),
+        sex: "",
+        phoneNumber: "",
+        assignedDoctor: "",
+        clientsGroup: [],
+        isNotSendTexting: false,
+      },
+      address: {
+        city: "",
+        country: "",
+        region: "",
+        postcode: "",
+        house: "",
+        street: "",
+      },
+      docs: {
+        type: "",
+        series: "",
+        number: "",
+        issuer: "",
+        issueDate: new Date(0),
+      },
+    };
   },
   validations() {
     return {};
