6dea367b939c83bbbebd2c55020552f846b77421
blur64 <shalomsi922@gmail.com>
2024-03-31 17:31:31 +0400

    add BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="button" :type="type">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    text: String,
+    type: String,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+@import "../styles/variables"
+
+.button
+  background-color: $accent
+  border: none
+  cursor: pointer
+  padding: 12px 16px
+  border-radius: 4px
+
+  &:hover
+    opacity: 0.8
+</style>
