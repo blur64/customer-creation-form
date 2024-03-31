293022b963de1c4c83e0df6832ecf9bcede5d407
blur64 <shalomsi922@gmail.com>
2024-03-31 16:30:50 +0400

    add form controls grid positions

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -2,150 +2,181 @@
   <form @submit="handleFormSubmit">
     <h1 class="form-title">Форма создания клиента</h1>
     <form-section :title="'Основная информация'">
-      <base-input
-        v-model="main.surname"
-        id="surname"
-        type="text"
-        label="Фамилия"
-        :errors="errorsMap.surname"
-      />
-      <base-input
-        v-model="main.name"
-        id="name"
-        type="text"
-        label="Имя"
-        :errors="errorsMap.name"
-      />
-      <base-input
-        v-model="main.patronymic"
-        id="patronymic"
-        type="text"
-        label="Отчество"
-      />
-      <base-input
-        v-model="main.birthDate"
-        id="birth-date"
-        type="date"
-        label="Дата рождения"
-        :errors="errorsMap.birthDate"
-      />
-      <base-select
-        v-model="main.sex"
-        id="sex"
-        label="Пол"
-        :options="[
-          { value: '0', text: 'М' },
-          { value: '1', text: 'Ж' },
-        ]"
-      />
-      <base-input
-        v-model="main.phoneNumber"
-        id="phone-number"
-        type="tel"
-        label="Номер телефона"
-        :errors="errorsMap.phoneNumber"
-      />
-      <base-select
-        v-model="main.assignedDoctor"
-        id="assigned-doctor"
-        label="Лечащий врач"
-        :options="[
-          { value: '0', text: 'Иванов' },
-          { value: '1', text: 'Захаров' },
-          { value: '2', text: 'Чернышева' },
-        ]"
-      />
-      <base-select
-        v-model="main.clientsGroup"
-        id="clents-group"
-        label="Группа клиентов"
-        :options="[
-          { value: '0', text: 'VIP' },
-          { value: '1', text: 'Проблемные' },
-          { value: '2', text: 'ОМС' },
-        ]"
-        :errors="errorsMap.clientsGroup"
-        multiple
-      />
-      <base-input
-        v-model="main.isNotSendTexting"
-        id="is-not-send-texting"
-        type="checkbox"
-        label="Не отправлять СМС"
-        :errors="errorsMap.isNotSendTexting"
-      />
+      <div class="form-controls-wrapper">
+        <base-input
+          v-model="main.surname"
+          id="surname"
+          type="text"
+          label="Фамилия"
+          :errors="errorsMap.surname"
+          class="cols-4"
+        />
+        <base-input
+          v-model="main.name"
+          id="name"
+          type="text"
+          label="Имя"
+          :errors="errorsMap.name"
+          class="cols-4"
+        />
+        <base-input
+          v-model="main.patronymic"
+          id="patronymic"
+          type="text"
+          label="Отчество"
+          class="cols-4"
+        />
+        <base-input
+          v-model="main.birthDate"
+          id="birth-date"
+          type="date"
+          label="Дата рождения"
+          :errors="errorsMap.birthDate"
+          class="cols-3"
+        />
+        <base-select
+          v-model="main.sex"
+          id="sex"
+          label="Пол"
+          :options="[
+            { value: '0', text: 'М' },
+            { value: '1', text: 'Ж' },
+          ]"
+          class="cols-1"
+        />
+        <base-input
+          v-model="main.phoneNumber"
+          id="phone-number"
+          type="tel"
+          label="Номер телефона"
+          :errors="errorsMap.phoneNumber"
+          class="cols-4"
+        />
+        <base-select
+          v-model="main.assignedDoctor"
+          id="assigned-doctor"
+          label="Лечащий врач"
+          :options="[
+            { value: '0', text: 'Иванов' },
+            { value: '1', text: 'Захаров' },
+            { value: '2', text: 'Чернышева' },
+          ]"
+          class="cols-4"
+        />
+        <base-select
+          v-model="main.clientsGroup"
+          id="clents-group"
+          label="Группа клиентов"
+          :options="[
+            { value: '0', text: 'VIP' },
+            { value: '1', text: 'Проблемные' },
+            { value: '2', text: 'ОМС' },
+          ]"
+          :errors="errorsMap.clientsGroup"
+          multiple
+          class="cols-4"
+        />
+        <base-input
+          v-model="main.isNotSendTexting"
+          id="is-not-send-texting"
+          type="checkbox"
+          label="Не отправлять СМС"
+          :errors="errorsMap.isNotSendTexting"
+          class="cols-4 texting-checkbox"
+        />
+      </div>
     </form-section>
     <form-section :title="'Адрес'">
-      <base-input
-        v-model="address.city"
-        id="city"
-        type="text"
-        label="Город"
-        :errors="errorsMap.city"
-      />
-      <base-input
-        v-model="address.country"
-        id="country"
-        type="text"
-        label="Страна"
-      />
-      <base-input
-        v-model="address.region"
-        id="region"
-        label="Область"
-        type="text"
-      />
-      <base-input
-        v-model="address.postcode"
-        id="postcode"
-        label="Индекс"
-        type="text"
-      />
-      <base-input v-model="address.house" id="house" label="Дом" type="text" />
-      <base-input
-        v-model="address.street"
-        id="street"
-        label="Улица"
-        type="text"
-      />
+      <div class="form-controls-wrapper">
+        <base-input
+          v-model="address.city"
+          id="city"
+          type="text"
+          label="Город"
+          :errors="errorsMap.city"
+          class="cols-4"
+        />
+        <base-input
+          v-model="address.country"
+          id="country"
+          type="text"
+          label="Страна"
+          class="cols-4"
+        />
+        <base-input
+          v-model="address.region"
+          id="region"
+          label="Область"
+          type="text"
+          class="cols-4"
+        />
+        <base-input
+          v-model="address.postcode"
+          id="postcode"
+          label="Индекс"
+          type="text"
+          class="cols-2"
+        />
+        <base-input
+          v-model="address.house"
+          id="house"
+          label="Дом"
+          type="text"
+          class="cols-2"
+        />
+        <base-input
+          v-model="address.street"
+          id="street"
+          label="Улица"
+          type="text"
+          class="cols-4"
+        />
+      </div>
     </form-section>
     <form-section :title="'Паспортные данные'">
-      <base-select
-        v-model="docs.type"
-        id="document-type"
-        label="Тип документа"
-        :options="[
-          { value: '0', text: 'Паспорт' },
-          { value: '1', text: 'Свидетельство о рождении' },
-          { value: '2', text: 'Вод. удостоверение' },
-        ]"
-        :errors="errorsMap.type"
-      />
-      <base-input
-        v-model="docs.series"
-        id="document-series"
-        label="Серия"
-        type="text"
-      />
-      <base-input
-        v-model="docs.number"
-        id="document-number"
-        label="Номер"
-        type="text"
-      />
-      <base-input
-        v-model="docs.issuer"
-        id="document-issuer"
-        label="Кем выдан"
-        type="text"
-      />
-      <base-input
-        v-model="docs.issueDate"
-        id="document-issue-date"
-        label="Дата выдачи"
-        type="date"
-        :errors="errorsMap.issueDate"
-      />
+      <div class="form-controls-wrapper">
+        <base-select
+          v-model="docs.type"
+          id="document-type"
+          label="Тип документа"
+          :options="[
+            { value: '0', text: 'Паспорт' },
+            { value: '1', text: 'Свидетельство о рождении' },
+            { value: '2', text: 'Вод. удостоверение' },
+          ]"
+          :errors="errorsMap.type"
+          class="cols-4"
+        />
+        <base-input
+          v-model="docs.series"
+          id="document-series"
+          label="Серия"
+          type="text"
+          class="cols-4"
+        />
+        <base-input
+          v-model="docs.number"
+          id="document-number"
+          label="Номер"
+          type="text"
+          class="cols-4"
+        />
+        <base-input
+          v-model="docs.issuer"
+          id="document-issuer"
+          label="Кем выдан"
+          type="text"
+          class="cols-9"
+        />
+        <base-input
+          v-model="docs.issueDate"
+          id="document-issue-date"
+          label="Дата выдачи"
+          type="date"
+          :errors="errorsMap.issueDate"
+          class="cols-3"
+        />
+      </div>
     </form-section>
     <button type="submit">Сохранить</button>
   </form>
@@ -276,4 +307,24 @@ export default {
 .form-title
   font-weight: $fw-light
   font-size: $fs-big
+
+.form-controls-wrapper
+  display: grid
+  grid-template-columns: repeat(12, 1fr)
+  row-gap: 10px
+  column-gap: 20px
+
+.cols-9
+  grid-column: 9 span
+.cols-4
+  grid-column: 4 span
+.cols-3
+  grid-column: 3 span
+.cols-2
+  grid-column: 2 span
+.cols-1
+  grid-column: 1 span
+
+.texting-checkbox
+  align-self: center
 </style>
